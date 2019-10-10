61e20da9af66cff84fee6d223ef4a3f212fb62c5
vuchampion <vuchampion@csu.fullerton.edu>
2019-10-09 21:38:24 -0700

    Add files via upload

--- SuperMario.py
+++ SuperMario.py
@@ -2,17 +2,26 @@ import pygame, sys
 from pygame.locals import *
 from Settings import Settings
 from Background import Background
+from Button import Button
 
 class SuperMario:
     def __init__(self):
         pygame.init()
+        pygame.mixer.init()
+        self.game_active = False
         self.game_settings = Settings()
         self.screen = pygame.display.set_mode((self.game_settings.WIDTH, self.game_settings.HEIGHT), 0, 32)
         pygame.display.set_caption('Super Mario')
 
+        # SET BUTTONS
+        self.play_button = Button(self, "PLAY", self.game_settings.WIDTH/2 - 100, self.game_settings.HEIGHT/2)
+
         # SET SPRITES
         self.bg = Background(self, 'Resources/Images/level_1_background.png')
         self.bg_floor = Background(self, 'Resources/Images/level_1_floor.png')
+
+        # SET SOUNDS
+        self.lvl1_bg_music = pygame.mixer.Sound('Resources/Sounds/level_1_theme.wav')
 
     def check_events(self):
         for event in pygame.event.get():
@@ -22,6 +31,9 @@ class SuperMario:
                 self.check_keydown(event)
             elif event.type == pygame.KEYUP:
                 self.check_keyup(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self.check_play_button(mouse_pos)
 
     def check_keydown(self, event):
         if event.key == pygame.K_ESCAPE:
@@ -42,15 +54,31 @@ class SuperMario:
             self.bg.moving_left = False
             self.bg_floor.moving_left = False
 
+    def check_play_button(self, mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.game_active:
+            self.game_active = True
+            pygame.mouse.set_visible(False)
+
+    def initialize(self):
+        self.bg.draw_bg()
+        self.bg_floor.draw_bg()
+        self.lvl1_bg_music.play()
+
     def run_game(self):
         while True:
             pygame.display.update()
             self.screen.fill((0, 0, 0))
-            self.check_events()
-            self.bg.draw_bg()
-            self.bg_floor.draw_bg()
+            self.play_button.draw_button()
+
+            if self.game_active == True:
+                self.initialize()
+
+            # update/check methods
             self.bg.update_bg()
             self.bg_floor.update_bg()
+
+            self.check_events()
 
 if __name__ == '__main__':
     game = SuperMario()
