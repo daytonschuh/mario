833e7e85e5894477fc7a60fd9a571422586242da
vuchampion <vuchampion@csu.fullerton.edu>
2019-10-23 21:47:44 -0700

    Add files via upload

--- Scoring.py
+++ Scoring.py
@@ -1,23 +1,42 @@
 import pygame
 from Text import *
+from Timer import *
 
 class Scoring:
     def __init__(self, screen, score, world, coins, time):
         self.screen = screen
+        self.clock = Timer(60)
         self.score_text = Text(self)
+        self.score_num_text = Text(self)
         self.world_text = Text(self)
+        self.world_num_text = Text(self)
         self.coins_text = Text(self)
         self.timer_text = Text(self)
+        self.timer_num_text = Text(self)
         self.scores = score
         self.world = world
         self.coins = coins
         self.timer = time
 
     def draw_text(self):
-        self.score_text.message('SCORE ' + str(self.scores), (255, 255, 255), 425, 20)
-        self.world_text.message('WORLD ' + str(self.world), (255, 255, 255), 300, 20)
-        self.coins_text.message('COINS ' + str(self.coins), (255, 255, 255), 200, 20)
-        self.timer_text.message('TIME ' + str(self.timer), (255, 255, 255), 100, 20)
+        self.score_num_text.message(str(self.scores), (255, 255, 255), 100, 30)
+        self.score_text.message('MARIO', (255, 255, 255), 100, 10)
+        self.world_text.message('WORLD', (255, 255, 255), 675, 10)
+        self.world_num_text.message(str(self.world), (255, 255, 255), 675, 30)
+        self.coins_text.message('COIN*' + str(self.coins), (255, 255, 255), 350, 20)
+        self.timer_text.message('TIME', (255, 255, 255), 900, 10)
+        self.timer_num_text.message(str(self.timer), (255, 255, 255), 900, 30)
 
     def update_text(self):
-        pass
+        self.run_clock()
+
+    def add_coin(self):
+        self.coins += 1
+
+    def add_score(self, score):
+        self.scores += score
+
+    def run_clock(self):
+        self.clock.tick()
+        if self.clock.check():
+            self.timer -= 1
