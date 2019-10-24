d7f94f381b7b314527b384b424e87fd9630696de
vuchampion <vuchampion@csu.fullerton.edu>
2019-10-23 17:50:00 -0700

    Update LevelCreator.py

--- LevelCreator.py
+++ LevelCreator.py
@@ -1,9 +1,8 @@
 from Level import Level
-from level_blocks import *
 
 def world_1_1(screen, settings):
     world_1_level_1 = \
-        Level(screen, settings, 'Resources/images/level_1_background.png', 'Resources/images/level_1_floor.png', (500, 600), (120,120), 500)
+        Level(screen, settings, 'Resources/Images/level_1_background.png', 'Resources/Images/level_1_floor.png', (500, 600), (120,120), 500)
 #    tiles = place_tiles(screen, settings, 0, 0, 400, 4)
 #    place_enemy()
 #    place_block()
